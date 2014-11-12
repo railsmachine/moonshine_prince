4c64d727cc72dc52c32e146a91b96ecf5156eecb
Dustin Beason <dustin@railsmachine.com>
2014-11-12 20:00:09 +0000

    cleaning up unnecessary spaces and remaining source info

--- lib/moonshine/prince.rb
+++ lib/moonshine/prince.rb
@@ -29,34 +29,28 @@ module Moonshine
 
       package_name = "prince_#{version}-5_ubuntu#{os_version}_amd64.deb"
 
-        exec "download prince",
-          :creates => "/usr/local/src/#{package_name}",
-          :command => "wget http://www.princexml.com/download/#{package_name}",
-          :cwd => "/usr/local/src",
-          :require => package("wget")
+      exec "download prince",
+        :creates => "/usr/local/src/#{package_name}",
+        :command => "wget http://www.princexml.com/download/#{package_name}",
+        :cwd => "/usr/local/src",
+        :require => package("wget")
 
-        package "prince",
-          :ensure => :installed,
-          :provider => :dpkg,
-          :source => "/usr/local/src/#{package_name}",
-          :require => [
-            exec("download prince"),
-            package("libgif4")
-          ]
-
-        if options[:license_file_path]
-          file "/usr/lib/prince/license/license.dat",
-            :ensure => :present,
-            :source => options[:license_file_path],
-            :require => package("prince")
-        end
+      package "prince",
+        :ensure => :installed,
+        :provider => :dpkg,
+        :source => "/usr/local/src/#{package_name}",
+        :require => [
+          exec("download prince"),
+          package("libgif4")
+        ]
 
       if options[:license_file_path]
-        file "/usr/local/lib/prince/license/license.dat",
+        file "/usr/lib/prince/license/license.dat",
           :ensure => :present,
           :source => options[:license_file_path],
-          :require => exec("install prince")
+          :require => package("prince")
       end
+
     end
   end
 end
